8d39540ceea054e09dcf76f3664eed2d9d19db71
Julianne <juliannelicon@mpto.mp.br>
2023-08-16 14:24:57 -0300

    Projeto Inicial

---
+++ lib/src/barbershop_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BarbershopApp extends StatelessWidget {
+
+  const BarbershopApp({ super.key });
+
+   @override
+   Widget build(BuildContext context) {
+       return Scaffold(
+           appBar: AppBar(title: const Text('DW'),),
+           body: Container(),
+       );
+  }
+}
