14f143396156badd8a8e2a7ed9420b2276e64953
Julianne <juliannelicon@mpto.mp.br>
2023-08-16 14:53:03 -0300

    Adicionando AsyncState e Loader

--- lib/src/barbershop_app.dart
+++ lib/src/barbershop_app.dart
@@ -1,14 +1,25 @@
+import 'package:asyncstate/widget/async_state_builder.dart';
+import 'package:dw_barbershop/src/core/widgets/barbershop_loader.dart';
+import 'package:dw_barbershop/src/features/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
 class BarbershopApp extends StatelessWidget {
 
   const BarbershopApp({ super.key });
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('DW'),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return AsyncStateBuilder(
+      customLoader: const BarbershopLoader(),
+      builder: (asyncNavigatorObserver) {
+        return MaterialApp(
+          title: 'DW Barbershop',
+          navigatorObservers: [asyncNavigatorObserver],
+          routes: {
+            '/': (_) => const SplashPage()
+          },
+        );
+      },
+    );
   }
 }
