66401d2b1b341260718bdd9262e46266fa65118c
Julianne <juliannelicon@mpto.mp.br>
2023-08-16 16:23:39 -0300

    Adicionando animação na página splash

--- lib/src/features/splash/splash_page.dart
+++ lib/src/features/splash/splash_page.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  var _scale = 10.0;
+  var _animationOpacityLogo = 0.0;
+
+  double get _logoAnimationWidth => 100 * _scale;
+  double get _logoAnimationHeight => 120 * _scale;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        _animationOpacityLogo = 1.0;
+        _scale = 1;
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,15 +32,27 @@ class SplashPage extends StatelessWidget {
       body: DecoratedBox(
         decoration: const BoxDecoration(
           image: DecorationImage(
-            image: AssetImage(
-              'assets/images/background_image_chair.jpg',
-            ),
-            opacity: 0.2,
-            fit: BoxFit.cover
-          ),
+              image: AssetImage(
+                'assets/images/background_image_chair.jpg',
+              ),
+              opacity: 0.2,
+              fit: BoxFit.cover),
         ),
         child: Center(
-          child: Image.asset('assets/images/imgLogo.png'),
+          child: AnimatedOpacity(
+            duration: const Duration(seconds: 3),
+            curve: Curves.easeIn,
+            opacity: _animationOpacityLogo,
+            child: AnimatedContainer(
+                duration: const Duration(seconds: 3),
+                width: _logoAnimationWidth,
+                height: _logoAnimationHeight,
+                curve: Curves.linearToEaseOut,
+                child: Image.asset(
+                  'assets/images/imgLogo.png',
+                  fit: BoxFit.cover,
+                )),
+          ),
         ),
       ),
     );
